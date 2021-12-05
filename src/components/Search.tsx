0745338389a5c24738a3d06c6169207ee7ecf0ca
Daniil Markov <mrmrkda@gmail.com>
2021-12-05 15:57:46 +0700

    feat: zeroing cursor on input change

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,6 +4,7 @@ import {
   $gameId,
   searchFx,
   setBroadcasterId,
+  setCursor,
   setGameId,
 } from 'models/search'
 
@@ -11,23 +12,37 @@ export default function Search() {
   const gameId = useStore($gameId)
   const broadcasterId = useStore($broadcasterId)
 
+  const handleChangeBoradcasterId = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setBroadcasterId(e.target.value)
+    setCursor('')
+  }
+  const handleChangeGameId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGameId(e.target.value)
+    setCursor('')
+  }
+  const handleSearchClick = () => {
+    searchFx()
+  }
+
   return (
     <div className="flex justify-center my-4 space-x-4">
       <input
         className="pl-2 py-1 rounded-md shadow"
         type="text"
-        onChange={(e) => setBroadcasterId(e.target.value)}
+        onChange={handleChangeBoradcasterId}
         placeholder="Broadcaster ID"
         value={broadcasterId}
       />
       <input
         className="pl-2 py-1 rounded-md shadow"
         type="text"
-        onChange={(e) => setGameId(e.target.value)}
+        onChange={handleChangeGameId}
         placeholder="Game ID"
         value={gameId}
       />
-      <button className="px-2 rounded-md shadow" onClick={() => searchFx()}>
+      <button className="px-2 rounded-md shadow" onClick={handleSearchClick}>
         Search
       </button>
     </div>
