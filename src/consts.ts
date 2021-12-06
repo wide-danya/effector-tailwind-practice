e09bce113d366344b5cca9091ff99150e44eb057
Daniil Markov <mrmrkda@gmail.com>
2021-12-06 12:43:18 +0700

    chore: update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,13 @@
 export const TWITCH_CLIENT_ID = process.env.REACT_APP_TWITCH_CLIENT_ID ?? ''
 export const TWITCH_BEARER = process.env.REACT_APP_TWITCH_BEARER ?? ''
+
+export const DEFAULT_BROADCASTER_ID = ''
+export const DEFAULT_GAME_ID = '509663'
+export const DEFAULT_CURSOR = ''
+
+export const AXIOS_CONFIG = {
+  headers: {
+    'Client-ID': TWITCH_CLIENT_ID,
+    Authorization: TWITCH_BEARER,
+  },
+}
